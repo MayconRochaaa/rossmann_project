8fc4de4ff5c051162c237ae9610dfb921abb6509
Maycon Rocha <mayconrochads@gmail.com>
2023-06-09 15:13:45 -0300

    Ajustes gerais

--- api/rossmann/Rossmann.py
+++ api/rossmann/Rossmann.py
@@ -15,11 +15,9 @@ def is_promo(row):
 
 class Rossmann (object):
     
-    
-    
-    
     def __init__(self):
         self.home_path = '/home/mayconr/repos/ComunidadeDS/dsproducao/rossmann_project/'
+        
         self.competition_time_month_scaler  = pickle.load(open(self.home_path + 'parameter/competition_time_month_scaler.pkl', 'rb'))
         self.competition_distance_scaler    = pickle.load(open(self.home_path + 'parameter/competition_distance_scaler.pkl', 'rb'))
         self.promo_time_week_scaler         = pickle.load(open(self.home_path + 'parameter/promo_time_week_scaler.pkl', 'rb'))
